aa645af7babdf2c0d3fed5b033827f4527602ea0
mariepzn <125984433+mariepzn@users.noreply.github.com>
2024-04-11 20:43:47 +0000

    page info et custom salade fraiche

--- js/custom-provence.js
+++ js/custom-provence.js
@@ -1,16 +1,17 @@
+let totalPrice = 10;
 document.addEventListener('DOMContentLoaded', () => {
     const addButton = document.querySelectorAll('.btn-add');
     const subtractButton = document.querySelectorAll('.btn-sub');
     const totalPriceElement = document.querySelector('footer p');
-
+    
     // actualiser le prix total du produit
     function updateTotalPrice() {
-        let totalPrice = 6;
+        
         document.querySelectorAll('.quantity').forEach(quantityElement => {
             const id = quantityElement.getAttribute('data-id');
             const price = parseFloat(document.querySelector('.btn-add[data-id="' + id + '"]').getAttribute('data-price'));
             const quantity = parseInt(quantityElement.textContent);
-            totalPrice += price * quantity;
+            totalPrice += price * (quantity-1);
         });
         totalPriceElement.textContent = `Total : ${totalPrice.toFixed(2)}€`;
     }
@@ -37,3 +38,21 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+function addToCart() {
+    const platId = "3";
+    const platName = "provencale";
+    const platPrice = totalPrice.toString();
+    
+    addItemToCart(platId, platName, platPrice);
+    window.location.href = "menu.html";
+
+}
+function addItemToCart(id, name, price) {
+    let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {};
+    if (cart[id]) {
+        cart[id].quantity += 1;
+    } else {
+        cart[id] = {name: name, price: price, quantity: 1};
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
